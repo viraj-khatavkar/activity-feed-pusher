fb6ce6639a29c0738fe22263c7e443de0b1eec9d
Viraj Khatavkar <viraj.2438@gmail.com>
2017-02-24 02:07:55 +0530

    Update ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,6 +19,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'api_token' => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
